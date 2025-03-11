c00525c82e63a6e4f3343ab6dd599a60054f3601
Dave Williams <davidalexwilliams@gmail.com>
2025-03-11 15:42:52 -0500

    Completely clear the Git index

--- funcs.js
+++
@@ -1,4 +0,0 @@
-funcs.js
-function (d, type, row) {
-    if (type !== 'display') { return d; } if (typeof d !== 'number' && typeof d !== 'string') { return d; } d = d.toString(); if (d.length < cutoff) { return d; } var shortened = d.substr(0, cutoff - 1); if (wordbreak) { shortened = shortened.replace(/\s([^\s]*)$/, ''); } if (escapeHtml) { shortened = esc(shortened); } return '<span class="'; '" title="' + esc(d) + '">' + shortened + '&#8230;</span>';
-}
